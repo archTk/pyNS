03c8e042356afad48eac5f98dc3ee808a92b9c5f
DaroN1337 <simone.manini@gmail.com>
2012-05-23 16:55:10 +0200

    fixed bessel setup and its readme

--- setup.py
+++ setup.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python
 
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    print "Please install Cython"
+    raise
+
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import os
 
-
-CLASSIFIERS = ["Development Status :: 1 - Beta",
+CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
                "Environment :: Console",
                "Intended Audience :: Science/Research",
                "License :: OSI Approved :: BSD License",
-               "Operating System :: Linux/MacOsX",
+               "Operating System :: MacOS",
+               "Operating System :: POSIX :: Linux",
                "Programming Language :: Cython",
                "Programming Language :: Python",
                "Topic :: Scientific/Engineering"]
 
 # Description
 description = "Bessel functions of the first kind written in Cython"
-fid = file('README.rst', 'r')
+fid = file('README_BESSEL.txt', 'r')
 long_description = fid.read()
 fid.close()
 idx = max(0, long_description.find("Bessel functions of the first kind"))
 long_description = long_description[idx:]
 
-NAME                = 'jBessels'
+NAME                = 'jBessel'
 MAINTAINER          = "Simone Manini"
 MAINTAINER_EMAIL    = "simone.manini@gmail.com"
 DESCRIPTION         = description
 LONG_DESCRIPTION    = long_description
-URL                 = "http://berkeleyanalytics.com/bottleneck"
-DOWNLOAD_URL        = "http://pypi.python.org/pypi/jBessels"
+URL                 = "https://github.com/daron1337/jBessel"
+DOWNLOAD_URL        = "http://pypi.python.org/pypi/jBessel"
 LICENSE             = "BSD"
 CLASSIFIERS         = CLASSIFIERS
 AUTHOR              = "Simone Manini"
 AUTHOR_EMAIL        = "simone.manini@gmail.com"
 PLATFORMS           = "Linux/MacOsX"
-ISRELEASED          = False
-VERSION             = '0.1'
-PACKAGES            = ["jBessels"]
-PACKAGE_DATA        = {'jBessels': ['LICENSE']}
+ISRELEASED          = True
+VERSION             = '0.1.4'
 
 setup(name=NAME,
       maintainer=MAINTAINER,
@@ -53,8 +56,6 @@ setup(name=NAME,
       author_email=AUTHOR_EMAIL,
       platforms=PLATFORMS,
       version=VERSION,
-      packages=PACKAGES,
-      package_data=PACKAGE_DATA,
       cmdclass = {'build_ext': build_ext},
       ext_modules = [Extension("Bessel", ["Bessel.pyx"])]
      )
